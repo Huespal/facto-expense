4ef3220d47c0384987cb5bb334115ecb18226c80
Dani <daniel.sanluis@shopery.com>
2025-03-01 13:25:00 +0100

    feat: add Tenant selector

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,25 @@
+import { getTenantId } from '@/app/actions';
 import Header from '@/components/Header';
 import LoginForm from '@/components/LoginForm';
 import Section from '@/components/Section';
+import { tenants } from '@/core/constants';
 import Image from 'next/image';
 
 export default async function Login() {
+  const tenantId = (await getTenantId())?.value;
+  const tenant = tenants.find(tenant => tenant.id === tenantId);
+  const tenantLogo = tenant?.logo ?? tenants[0]?.logo;
+
   return (
     <>
-      <Header isAuthorized={false} />
+      <Header tenantId={tenantId} isAuthorized={false} />
       <Section>
         <Image
-          src="https://picsum.photos/seed/picsum/250/200"
+          src={tenantLogo}
           width={250}
           height={200}
           alt="Facto Expense"
+          priority
           style={{
             display: 'block',
             margin: '5rem auto'
---
+++ src/components/TenantSelect/index.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { setTenantId } from '@/app/actions';
+import FieldSelect from '@/components/FieldSelect';
+import { tenants } from '@/core/constants';
+import { ChangeEvent, useEffect } from 'react';
+
+interface HeaderProps {
+  initialValue?: string;
+}
+
+const TenantSelect = ({ initialValue }: HeaderProps) => {
+  const setTenant = (evt: ChangeEvent<HTMLSelectElement>) => {
+    const tenantId = evt.target.value;
+    setTenantId(tenantId);
+  }
+
+  useEffect(() => {
+    if (!initialValue) {
+      setTenantId(tenants[0].id);
+    }
+  }, [initialValue]);
+
+  return (
+    <FieldSelect
+      id="tenant-selector"
+      name="tenant"
+      value={initialValue}
+      options={tenants}
+      onChange={setTenant}
+    >
+      {({ id, name }) => (
+        <option key={id} value={id}>{name}</option>
+      )}
+    </FieldSelect>
+  );
+}
+
+export default TenantSelect;
