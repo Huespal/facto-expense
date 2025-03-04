985c4a9175a76387b29357b13c6d8a78260916e4
Huespal <Huespal@users.noreply.github.com>
2025-03-04 12:57:43 +0100

    feat: adapt tenant ids

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -10,17 +10,17 @@ export const tenantName = 'facto-expense-tenant';
 // I decided to define a hardcoded list to allow a faster development. 
 export const tenants: Tenant[] = [
   {
-    id: 'tenant-1',
+    id: '1',
     name: 'Agència de viatges l\'espardenya',
     logo: 'https://picsum.photos/id/17/250/200' 
   },
   {
-    id: 'tenant-2',
+    id: '2',
     name: 'Rodamons',
     logo: 'https://picsum.photos/id/15/250/200'
   },
   {
-    id: 'tenant-3',
+    id: '3',
     name: 'Centre excursionista',
     logo: 'https://picsum.photos/id/28/250/200'
   }
