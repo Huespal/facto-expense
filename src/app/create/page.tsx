4843eda82ab417cb0ed2eb7a277992da791f9a9f
Huespal <Huespal@users.noreply.github.com>
2025-03-05 16:11:26 +0100

    feat: display current Tenant

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,11 +1,14 @@
+import { getTenantId } from '@/app/actions';
 import ExpenseForm from '@/components/Expense/Form';
 import Header from '@/components/Header';
 import Section from '@/components/Section';
 
 export default async function Create() {
+  const tenantId = (await getTenantId())?.value;
+
   return (
     <>
-      <Header isAuthorized={true} />
+      <Header isAuthorized={true} tenantId={tenantId} />
       <Section>
         <h1>Add expense</h1>
         <ExpenseForm />
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,6 +4,8 @@ import { deleteAccessToken } from '@/app/actions';
 import Button from '@/components/Button';
 import { HeaderStyled } from '@/components/Header/styles';
 import TenantSelect from '@/components/TenantSelect';
+import { tenants } from '@/core/constants';
+import { useQueryClient } from '@tanstack/react-query';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
@@ -14,15 +16,18 @@ interface HeaderProps {
 
 const Header = ({ tenantId, isAuthorized }: HeaderProps) => {
   const router = useRouter();
+  const queryClient = useQueryClient();
+  const tenant = tenants.find(tenant => tenant.id === tenantId);
 
   const logout = () => {
     deleteAccessToken();
+    queryClient.removeQueries();
     router.push('/login');
   }
 
   return (
     <HeaderStyled>
-      <h4><Link href="/">Facto Expense</Link></h4>
+      <h4><Link href="/">Facto Expense - {tenant?.name}</Link></h4>
       {isAuthorized && <Button onClick={logout}>Log out</Button>}
       {!isAuthorized && <TenantSelect initialValue={tenantId} />}
     </HeaderStyled>
